1d14b81fa9b97d57488b6ec25719eff774b4de5f
Jaras49 <jaras48@gmail.com>
2018-04-02 13:56:41 +0200

    Sort the Odd

---
+++ src/com/sorttheodd/Kata.java
@@ -0,0 +1,19 @@
+package com.sorttheodd;
+
+public class Kata {
+    public static int[] sortArray(int[] array) {
+
+        for (int i = 0; i < array.length -1; i++){
+
+
+            for (int j = i+1; j< array.length ; j++ ){
+                if (array[i] % 2 != 0 && array[j] % 2 != 0 && array[j] < array[i] && array[i] != 0 && array[j] != 0){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
